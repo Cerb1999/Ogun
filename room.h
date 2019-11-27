86ae69ed13ce137899f5bbd6fee805d7289e7286
Matthieu Labalette <labalett2u@fst-ats-312-l16.fst.site.univ-lorraine.fr>
2019-11-27 12:09:41 +0100

    starts

--- room.h
+++ room.h
@@ -1,5 +1,40 @@
 typedef struct room {
   int layout[][];
-  int cleared;
-  int occupied;
-} item_room, shop, boss_room;
+  bool cleared;
+  bool occupied;
+  int type
+} room;
+
+room cons(int[][] lay, int t) {
+  //room r = malloc;
+  r.layout = lay;
+  r.cleared = false;
+  r.occupied = false;
+  r.type = t;
+  return r;
+}
+
+int layout(room r) {
+  switch (r.type) {
+    case 0: // standart room
+      //spawn des mobs et du terrain en fonction du layout
+      break;
+    case 1: // item room
+      //piedestal avec objet au centre de la piece, possibilité de faire spawn des mobs
+      break;
+    case 2: // shop
+      //piedestaux avec hp, key et un objet ainsi que le prix de chacun, possibilité de faire spawn des mobs
+      break;
+    case 3: // boss room
+      //mob avec un indice particulier, les mobs spawn durant le combat
+      break;
+  }
+}
+
+void clear(room r) {
+  r.cleared = true;
+}
+
+void enter(room r) {
+  r.occupied = true;
+}
